09dcb46c2e785ecbe56c0bc100bf5bee9c85b26a
KingKoopa23JR <dylan.johnson1223@gmail.com>
2023-02-25 20:07:55 -0500

    UI tweaks and scroll to links added

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
@@ -8,4 +7,9 @@ import { Component } from '@angular/core';
 export class NavbarComponent {
   github: string = "https://github.com/dyjoh?tab=repositories";
   linkedin: string = "https://www.linkedin.com/in/dylan-johnson-927111208/";
+
+  onClickScroll(elementId: string): void {
+    console.log(elementId);
+    document.getElementById(elementId)?.scrollIntoView();
+  }
 }
